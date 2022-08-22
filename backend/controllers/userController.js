4de10e082274ef4baf817bf5603a762dcb8aeaec
ambili <ambilipakhd@gmail.com>
2022-08-22 16:14:22 +0530

    Rest API for users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,12 @@
 const asynchandler=require('express-async-handler')
+const User=require('../models/userModel')
 
 //get users
 const getUsers=asynchandler(async(req,res)=>{
-    res.status(200).json({message:"get users"})
+      
+    const users=await User.find() 
+
+    res.status(200).json(users)
 
 })
 //set user
@@ -11,7 +15,10 @@ const setUser=asynchandler(async(req,res)=>{
       res.status(400)
       throw new Error('Please add a name field')
     }
-    res.status(200).json({message:"set users"})
+    const user=await User.create({
+        name:req.body.name
+    })
+    res.status(200).json(user)
     
 })
 module.exports={
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,10 @@ const express=require('express')
 const dotenv=require('dotenv').config()
 const port=process.env.PORT || 5000
 const {errorHandler}=require('./middleware/errorMiddleware')
+const connectDB=require('./config/db')
 const app=express()
+
+connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
